f206b75500b736da265276f5a2cc6e1325a1d08f
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-03-04 19:32:15 -0700

    Initialize gitter

---
+++ README.md
@@ -0,0 +1,2 @@
+# MainTemplate/gitter
+A template I use
---
+++ gitter.bash
@@ -0,0 +1,15 @@
+# Put gitter here
+mkdir NAME
+cd NAME
+git clone -b stable https://github.com/HttpAnimation/NAME.git
+mv NAME stable
+git clone -b blank https://github.com/HttpAnimation/NAME.git
+mv NAME blank 
+git clone -b extra https://github.com/HttpAnimation/NAME.git
+mv NAME extra
+git clone -b gh-pages https://github.com/HttpAnimation/NAME.git
+mv NAME gh-pages
+git clone -b gitter https://github.com/HttpAnimation/NAME.git
+mv NAME gitter
+git clone -b main https://github.com/HttpAnimation/NAME.git
+mv NAME main
